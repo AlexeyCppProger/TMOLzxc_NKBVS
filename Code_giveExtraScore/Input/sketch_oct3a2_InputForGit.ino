9570b2bef36ec909ff8ff4a90997ff0c4156faba
Alexey <aleksejkurilkin021@gmail.com>
2025-10-05 12:15:59 +0300

    Update sketch_oct3a2_InputForGit.ino frontend

--- Code_giveExtraScore/Input/sketch_oct3a2_InputForGit.ino
+++ Code_giveExtraScore/Input/sketch_oct3a2_InputForGit.ino
@@ -3,17 +3,19 @@
 
 int DELAY = 100; 
 
+// Пины RGB-светодиода
 const int redPin = 12;
 const int bluePin = 22;
 const int greenPin = 23;
 
-
-String inputText = "";
+// Входные данные (текст)
+// String inputText = "Hello"; // по умолчанию
+String inputText = ""; // по умолчанию
 String bitStream;           // поток битов для трансляции
 
-
-const char* ssidAP = "ESP32_LEDZZZ";  // Имя WiFi сети
-const char* passwordAP = "12345678Z"; // пароль, минимум 8 символов
+// Настройки точки доступа
+const char* ssidAP = "TMOLzxc";  // Имя WiFi сети
+const char* passwordAP = "123456789"; // пароль, минимум 8 символов
 
 WebServer server(80);
 
@@ -40,30 +42,56 @@ void updateBitStream() {
   Serial.println(bitStream);
 }
 
+// void handleRoot() {
+//   String html = R"(<!doctype html><html lang=ru><head><meta charset=UTF-8><meta name=viewport content="width=device-width,initial-scale=1"><title>форма отправки сообщения</title><style>body{margin:0;height:100vh;display:flex;justify-content:center;align-items:center;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;background:linear-gradient(135deg,#74ebd5,#acb6e5);color:#333}form{background:rgba(255,255,255,.9);padding:30px 20px;border-radius:15px;box-shadow:0 8px 16px rgba(0,0,0,.2);max-width:500px;width:90%;display:flex;flex-direction:column;gap:20px}h1{margin:0;text-align:center;font-size:22px;color:#444}label{font-weight:600;display:flex;flex-direction:column;font-size:16px}.message-input{width:100%;padding:12px 16px;margin-top:8px;border:2px solid #ccc;border-radius:16px;font-size:14px;transition:border-color .2s,box-shadow .2s;box-sizing:border-box;min-height:100px;resize:vertical}.message-input:focus{border-color:#66afe9;outline:0;box-shadow:0 0 8px rgba(102,175,233,.6)}.radio-group{display:flex;flex-wrap:wrap;justify-content:space-around;padding:10px 0;background:linear-gradient(135deg,#f6d365,#fda085);border-radius:10px;box-shadow:inset 0 0 5px rgba(0,0,0,.1)}.radio-label{display:flex;align-items:center;font-size:14px;cursor:pointer;margin:5px auto 5px auto;transition:transform .2s}.radio-text{padding-top:2px}.radio-label:hover{transform:scale(1.05)}.radio-label input[type=radio]{display:none}.radio-custom{width:20px;height:20px;border:2px solid #555;border-radius:50%;margin-right:10px;position:relative;transition:all .2s}.radio-custom::after{content:"";position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:12px;height:12px;border-radius:50%;background-color:transparent;transition:background-color .2s}input[type=radio]:checked+.radio-custom{border-color:#6a5acd}input[type=radio]:checked+.radio-custom::after{background-color:#6a5acd}button{padding:14px;font-size:16px;background:linear-gradient(135deg,#89f7fe,#66a6ff);border:none;border-radius:10px;color:#fff;cursor:pointer;box-shadow:0 4px 8px rgba(0,0,0,.2);transition:background .3s,transform .2s}button:hover{transform:scale(1.05);background:linear-gradient(135deg,#66a6ff,#89f7fe)}.radio-item{display:flex;align-items:center;margin-bottom:8px;font-size:15px;font-weight:700}@media (max-width:600px){h1{font-size:20px}label{font-size:16px}.message-input{font-size:15px;padding:14px 18px;min-height:120px}.radio-group{display:flex;flex-direction:column;align-items:center}.radio-label{margin-bottom:8px}}</style><script>
+// document.getElementById('myForm').addEventListener('submit', function(e) {
+//   e.preventDefault(); // отменить стандартную отправку
+
+//   const form = this;
+//   const formData = new FormData(form);
+
+//   fetch('/send', {
+//     method: 'POST',
+//     body: formData
+//   })
+//   .then(response => {
+//     if (response.ok) {
+//       // После успешной отправки — очищаем форму
+//       form.reset();
+//       // Можно сделать редирект, если нужно:
+//       // window.location.href = '/';
+//     } else {
+//       alert('Ошибка при отправке формы');
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Ошибка:', error);
+//   });
+// });
+// </script></head><body><form id=myForm action=/send method=POST><h1>Отправка формы</h1><label>Текст сообщения: <textarea name=textInput class=message-input rows=4 required></textarea></label><div><p style=margin-bottom:8px;font-size:16px;font-weight:700>Скорость передачи информации:</p><div class=radio-group><div class=radio-item><label class=radio-label><input type=radio name=choice value=5 checked=checked> <span class=radio-custom></span></label><div><span class=radio-text>5 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=10> <span class=radio-custom></span></label><div><span class=radio-text>10 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=15> <span class=radio-custom></span></label><div><span class=radio-text>15 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=20> <span class=radio-custom></span></label><div><span class=radio-text>20 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=25> <span class=radio-custom></span></label><div><span class=radio-text>25 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=30> <span class=radio-custom></span></label><div><span class=radio-text>30 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=35> <span class=radio-custom></span></label><div><span class=radio-text>35 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=40> <span class=radio-custom></span></label><div><span class=radio-text>40 символов в секунду</span></div></div></div></div><button type=submit>Отправить</button></form></body></html>)";
+//   server.send(200, "text/html", html);
+// }
+
 void handleRoot() {
-  String html = R"(<!doctype html><html lang=ru><head><meta charset=UTF-8><meta name=viewport content="width=device-width,initial-scale=1"><title>форма отправки сообщения</title><style>body{margin:0;height:100vh;display:flex;justify-content:center;align-items:center;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;background:linear-gradient(135deg,#74ebd5,#acb6e5);color:#333}form{background:rgba(255,255,255,.9);padding:30px 20px;border-radius:15px;box-shadow:0 8px 16px rgba(0,0,0,.2);max-width:500px;width:90%;display:flex;flex-direction:column;gap:20px}h1{margin:0;text-align:center;font-size:22px;color:#444}label{font-weight:600;display:flex;flex-direction:column;font-size:16px}.message-input{width:100%;padding:12px 16px;margin-top:8px;border:2px solid #ccc;border-radius:16px;font-size:14px;transition:border-color .2s,box-shadow .2s;box-sizing:border-box;min-height:100px;resize:vertical}.message-input:focus{border-color:#66afe9;outline:0;box-shadow:0 0 8px rgba(102,175,233,.6)}.radio-group{display:flex;flex-wrap:wrap;justify-content:space-around;padding:10px 0;background:linear-gradient(135deg,#f6d365,#fda085);border-radius:10px;box-shadow:inset 0 0 5px rgba(0,0,0,.1)}.radio-label{display:flex;align-items:center;font-size:14px;cursor:pointer;margin:5px auto 5px auto;transition:transform .2s}.radio-text{padding-top:2px}.radio-label:hover{transform:scale(1.05)}.radio-label input[type=radio]{display:none}.radio-custom{width:20px;height:20px;border:2px solid #555;border-radius:50%;margin-right:10px;position:relative;transition:all .2s}.radio-custom::after{content:"";position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:12px;height:12px;border-radius:50%;background-color:transparent;transition:background-color .2s}input[type=radio]:checked+.radio-custom{border-color:#6a5acd}input[type=radio]:checked+.radio-custom::after{background-color:#6a5acd}button{padding:14px;font-size:16px;background:linear-gradient(135deg,#89f7fe,#66a6ff);border:none;border-radius:10px;color:#fff;cursor:pointer;box-shadow:0 4px 8px rgba(0,0,0,.2);transition:background .3s,transform .2s}button:hover{transform:scale(1.05);background:linear-gradient(135deg,#66a6ff,#89f7fe)}.radio-item{display:flex;align-items:center;margin-bottom:8px;font-size:15px;font-weight:700}@media (max-width:600px){h1{font-size:20px}label{font-size:16px}.message-input{font-size:15px;padding:14px 18px;min-height:120px}.radio-group{display:flex;flex-direction:column;align-items:center}.radio-label{margin-bottom:8px}}</style><script>
-document.getElementById('myForm').addEventListener('submit', function(e) {
-  e.preventDefault(); // отменить стандартную отправку
-
-  const form = this;
-  const formData = new FormData(form);
-
-  fetch('/send', {
-    method: 'POST',
-    body: formData
-  })
-  .then(response => {
-    if (response.ok) {
-      form.reset();
-    } else {
-      alert('Ошибка при отправке формы');
-    }
-  })
-  .catch(error => {
-    console.error('Ошибка:', error);
-  });
-});
-</script></head><body><form id=myForm action=/send method=POST><h1>Отправка формы</h1><label>Текст сообщения: <textarea name=textInput class=message-input rows=4 required></textarea></label><div><p style=margin-bottom:8px;font-size:16px;font-weight:700>Скорость передачи информации:</p><div class=radio-group><div class=radio-item><label class=radio-label><input type=radio name=choice value=5 checked=checked> <span class=radio-custom></span></label><div><span class=radio-text>5 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=10> <span class=radio-custom></span></label><div><span class=radio-text>10 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=15> <span class=radio-custom></span></label><div><span class=radio-text>15 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=20> <span class=radio-custom></span></label><div><span class=radio-text>20 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=25> <span class=radio-custom></span></label><div><span class=radio-text>25 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=30> <span class=radio-custom></span></label><div><span class=radio-text>30 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=35> <span class=radio-custom></span></label><div><span class=radio-text>35 символов в секунду</span></div></div><div class=radio-item><label class=radio-label><input type=radio name=choice value=40> <span class=radio-custom></span></label><div><span class=radio-text>40 символов в секунду</span></div></div></div></div><button type=submit>Отправить</button></form></body></html>)";
+  String html = R"(<!DOCTYPE html><html lang="ru"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>форма отправки сообщения</title><style>body{margin:0;height:100vh;display:flex;justify-content:center;align-items:center;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;background:linear-gradient(135deg,#ededed,#e74d3cdf);color:#333}form{background:#fff;padding:30px 20px;border-radius:15px;box-shadow:16px 16px 32px rgba(0,0,0,.685);max-width:500px;width:90%;display:flex;flex-direction:column;gap:20px}h1{margin:0;text-align:center;font-size:22px;color:#444}label{font-weight:600;display:flex;flex-direction:column;font-size:16px;color:#222}.message-input{width:100%;padding:12px 16px;margin-top:8px;border:2px solid #aaa;border-radius:16px;font-size:14px;transition:border-color .2s,box-shadow .2s;box-sizing:border-box;min-height:100px;resize:vertical;box-shadow:0 0 2px rgba(0,0,0,.685)}.message-input:focus{border-color:#e74c3c;outline:0;box-shadow:0 0 8px rgba(231,76,60,.6)}.radio-group{display:flex;flex-wrap:wrap;justify-content:space-around;padding:10px 0;background:#f0f0f0;border-radius:10px;box-shadow:8px 8px 16px rgba(0,0,0,.685)}.radio-label{display:flex;align-items:center;font-size:14px;cursor:pointer;margin:5px auto;transition:transform .2s;color:#222}.radio-text{padding-top:2px}.radio-label:hover{transform:scale(1.05)}.radio-label input[type=radio]{display:none}.radio-custom{width:20px;height:20px;border:2px solid #555;border-radius:50%;margin-right:10px;position:relative;transition:all .2s}.radio-custom::after{content:"";position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:12px;height:12px;border-radius:50%;background-color:transparent;transition:background-color .2s}input[type=radio]:checked+.radio-custom{border-color:#e74c3c}input[type=radio]:checked+.radio-custom::after{background-color:#e74c3c}button{padding:14px;font-size:16px;background:#e74c3c;border:none;border-radius:10px;color:#fff;cursor:pointer;box-shadow:8px 8px 16px rgba(0,0,0,.685);transition:background .3s,transform .2s}button:hover{transform:scale(1.05);background:#c0392b}.radio-item{display:flex;align-items:center;margin-bottom:8px;font-size:15px;font-weight:700}@media (max-width:600px){h1{font-size:20px}label{font-size:16px}.message-input{font-size:15px;padding:14px 18px;min-height:120px}.radio-group{display:flex;flex-direction:column;align-items:center}.radio-label{margin-bottom:8px}}</style><script>document.getElementById('myForm').addEventListener('submit', function(e) {
+      e.preventDefault(); // отменить стандартную отправку
+      const form = this;
+      const formData = new FormData(form);
+      fetch('/send', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => {
+        if (response.ok) {
+          form.reset();
+        } else {
+          alert('Ошибка при отправке формы');
+        }
+      })
+      .catch(error => {
+        console.error('Ошибка:', error);
+      });
+    });</script></head><body><form id="myForm" action="/send" method="POST"><h1>Отправка формы</h1><label>Текст сообщения: <textarea name="textInput" class="message-input" rows="4" required></textarea></label><div><p style="margin-bottom:8px;font-size:16px;font-weight:700">Скорость передачи информации:</p><div class="radio-group"><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="5" checked> <span class="radio-custom"></span></label><div><span class="radio-text">5 символов в секунду</span></div></div><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="10"> <span class="radio-custom"></span></label><div><span class="radio-text">10 символов в секунду</span></div></div><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="15"> <span class="radio-custom"></span></label><div><span class="radio-text">15 символов в секунду</span></div></div><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="20"> <span class="radio-custom"></span></label><div><span class="radio-text">20 символов в секунду</span></div></div><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="25"> <span class="radio-custom"></span></label><div><span class="radio-text">25 символов в секунду</span></div></div><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="30"> <span class="radio-custom"></span></label><div><span class="radio-text">30 символов в секунду</span></div></div><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="35"> <span class="radio-custom"></span></label><div><span class="radio-text">35 символов в секунду</span></div></div><div class="radio-item"><label class="radio-label"><input type="radio" name="choice" value="40"> <span class="radio-custom"></span></label><div><span class="radio-text">40 символов в секунду</span></div></div></div></div><button type="submit">Отправить</button></form></body></html>)";
   server.send(200, "text/html", html);
 }
 
@@ -76,6 +104,7 @@ void handleSend() {
     updateBitStream();
 
     DELAY = 1000 / server.arg("choice").toInt();
+    // server.send(200, "text/plain", "Текст получен: " + inputText);
 
     server.sendHeader("Location", "/");
     server.send(302, "text/plain", "");
